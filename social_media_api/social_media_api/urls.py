34d135037d7bf2045260a8425aec9cd4d1fbc96b
tsion1622 <tayu.16.enye@gmail.com>
2025-08-30 16:26:37 +0300

    added path

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from posts.views import feed
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
