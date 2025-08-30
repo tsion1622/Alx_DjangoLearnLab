2a5074b5211b2afa94233501ac7b7edd534ae1f8
tsion1622 <tayu.16.enye@gmail.com>
2025-08-30 15:31:20 +0300

    Implement Posts and Comments Functionality

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,40 @@
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .serializers import PostSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets, filters
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+class PostPagination(PageNumberPagination):
+    page_size = 10  # number of posts per page
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = PostPagination
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'content']  # allow searching posts by title or content
+    ordering_fields = ['created_at', 'updated_at']
+    ordering = ['-created_at']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
